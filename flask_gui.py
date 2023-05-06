9a043d478589029ead1e7eb297124cb8ceea0175
syncsyncsync <kayak.confine-0x@icloud.com>
2023-05-06 16:34:06 +0900

    gui add

--- flask_gui.py
+++ flask_gui.py
@@ -8,15 +8,34 @@ chat_models = [
     "gpt-3.5-turbo", "gpt-3.5-turbo-0301"
 ]
 
+# @app.route('/', methods=['GET', 'POST'])
+# def chat():
+#     if request.method == 'POST':
+#         messages = []
+#         messages.append({"role": "user", "content": request.form['user_input']})
+#         selected_model = request.form['model']
+#         response = chat_with_models(selected_model, messages)
+#         return render_template('index.html', response=response, models=chat_models)
+#     return render_template('index.html', models=chat_models)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
         messages = []
-        messages.append({"role": "user", "content": request.form['user_input']})
+        user_message = request.form['user_input']
+        messages.append({"role": "user", "content": user_message})
         selected_model = request.form['model']
-        response = chat_with_models(selected_model, messages)
+        
+        # Call the send_messages function from graphQL_client.py
+        send_messages_response = send_messages(user_message=user_message)
+        
+        # Adjust the format of the response to match the GraphQL client response
+        response = {"choices": [{"message": {"role": "assistant", "content": send_messages_response["assistantMessage"]["content"]}}]}
+        
         return render_template('index.html', response=response, models=chat_models)
     return render_template('index.html', models=chat_models)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=6000)
+
