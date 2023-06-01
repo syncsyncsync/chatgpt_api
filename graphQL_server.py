97400f971b60838d64c14ab05e9d8d6ab80fdbdb
syncsyncsync <kayak.confine-0x@icloud.com>
2023-06-01 20:44:51 +0900

    filter apply

--- graphQL_server.py
+++ graphQL_server.py
@@ -9,15 +9,27 @@ import spacy
 from chatgpt import chat_with_models
 # import MeCab
 import datetime
+from collections import defaultdict
+
+nlp = spacy.load("en_core_web_sm")
+
 # Initializing session history
 session_history = {}
 
 
-def load_spacy_model():
-    global nlp
-    nlp = spacy.load("en_core_web_sm")
+# def load_spacy_model():
+#     global nlp
+#     nlp = spacy.load("en_core_web_sm")
 
-load_spacy_model()
+# load_spacy_model()
+
+
+def find_names_and_places(text):
+    doc = nlp(text)
+    names = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+    places = [ent.text for ent in doc.ents if ent.label_ == "GPE"]
+
+    return names, places
 
 class SystemMessage(graphene.ObjectType):
     content = graphene.String()
@@ -29,27 +41,71 @@ class UserMessage(graphene.ObjectType):
     content = graphene.String()
 
 
+def unique_update(original_dict, new_dict):
+    for key, value in new_dict.items():
+        if key in original_dict:
+            counter = 1
+            new_key = key + "_" + str(counter)
+            while new_key in original_dict:
+                counter += 1
+                new_key = key + "_" + str(counter)
+            original_dict[new_key] = value
+        else:
+            original_dict[key] = value
+    return original_dict
+
+
 def filter_keyword_information(text, keyword_file='keywords.txt'):
+    replacements = {}
     with open(keyword_file, 'r', encoding='utf-8') as file:
         keywords = [line.strip() for line in file.readlines()]
-    
+
     for keyword in keywords:
-        text = text.replace(keyword, "{REDACTED}")
+        if keyword in text:
+            text = text.replace(keyword, "{REDACTED}")
+            replacements[keyword] = "{REDACTED}"
 
-    return text
+    return text, replacements
 
+def filter_numerical_information(text):
+    replacements = defaultdict(list)
 
-def filter_confidential_by_ai(text, model):
-    # to be deployed 
-    return text
+    # Replace phone numbers
+    pattern = r'\b\d{2,4}[-\s.]?\d{2,4}[-\s.]?\d{2,4}[-\s.]?\d{0,4}\b'
+    for match in re.findall(pattern, text):
+        replacements[match].append('{phone_number}')
+    text = re.sub(pattern, '{phone_number}', text)
+    
+    # Replace credit card numbers
+    pattern = r'\b(?:\d{4}[-\s.]?){3}\d{4}\b'
+    for match in re.findall(pattern, text):
+        replacements[match].append('{credit_card_number}')
+    text = re.sub(pattern, '{credit_card_number}', text)
+    
+    # Replace expiration dates
+    pattern = r'\b\d{2}\/\d{2,4}\b'
+    for match in re.findall(pattern, text):
+        replacements[match].append('{expiration_date}')
+    text = re.sub(pattern, '{expiration_date}', text)
 
+    return text, replacements
 
-def find_names_and_places(text):
-    doc = nlp(text)
-    names = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
-    places = [ent.text for ent in doc.ents if ent.label_ == "GPE"]
+def filter_anonymize_text(text):
+    replacements = {}
+    names, places = find_names_and_places(text)
+    name_placeholder = "{name}"
+    place_placeholder = "{place}"
+    
+    for name in names:
+        text = re.sub(re.escape(name), name_placeholder, text, flags=re.IGNORECASE | re.UNICODE)
+        replacements[name] = name_placeholder
+    
+    for place in places:
+        text = re.sub(re.escape(place), place_placeholder, text, flags=re.IGNORECASE | re.UNICODE)
+        replacements[place] = place_placeholder
 
-    return names, places
+    return text, replacements
+
 
 
 def filter_japanese_names(text):
@@ -57,102 +113,164 @@ def filter_japanese_names(text):
     node = mecab.parseToNode(text)
 
     masked_text = text
+    replacements = {}
     while node:
         word, feature = node.surface, node.feature.split(',')
         if feature[0] == '名詞' and feature[1] == '固有名詞':
-            masked_text = re.sub(word, "{japanese_name}", masked_text)
+            masked_word = "{japanese_name}"
+            masked_text = masked_text.replace(word, masked_word)
+            if word not in replacements:
+                replacements[word] = masked_word
         node = node.next
 
-    return masked_text
+    return masked_text, replacements
 
 
 def filter_japanese_by_name_keisyou(text):
     name_pattern = r"([一-龯]+(さん|ちゃん|君|くん|様|さま))"
-    masked_text = re.sub(name_pattern, "{japanese_name}", text)
-    return masked_text
+    replacements = {}
+    masked_text = text
+    matches = re.findall(name_pattern, text)
+    for match in matches:
+        masked_word = "{japanese_name}"
+        masked_text = masked_text.replace(match[0], masked_word)
+        if match[0] not in replacements:
+            replacements[match[0]] = masked_word
+    return masked_text, replacements
+
 
 def filter_japanese_by_place_keisyou(text):
     place_pattern = r"([一-龯]+(市|町|村|区|都))"
-    masked_text = re.sub(place_pattern, "{japanese_place}", text)
-    return masked_text
+    replacements = {}
+    masked_text = text
+    matches = re.findall(place_pattern, text)
+    for match in matches:
+        masked_word = "{japanese_place}"
+        masked_text = masked_text.replace(match[0], masked_word)
+        if match[0] not in replacements:
+            replacements[match[0]] = masked_word
+    return masked_text, replacements
 
 
 def filter_japanese_by_age_keyshou(text):
     age_pattern = r"(\d{1,2}歳)"
-    masked_text = re.sub(age_pattern, "{japanese_age}", text)
-    return masked_text
-
-def filter_japanese_by_mecab(text):
-    mecab = MeCab.Tagger("-Ochasen")
-    node = mecab.parseToNode(text)
-
+    replacements = {}
     masked_text = text
-    while node:
-        word, feature = node.surface, node.feature.split(',')
-        if feature[0] == '名詞' and feature[1] == '固有名詞':
-            masked_text = re.sub(word, "{japanese_name}", masked_text)
-        node = node.next
-
-    return masked_text
-
-
-def filter_anonymize_text(text):
-    names, places = find_names_and_places(text)
-    name_placeholder = "{name}"
-    place_placeholder = "{place}"
-    
-    for name in names:
-        text = re.sub(re.escape(name), name_placeholder, text, flags=re.IGNORECASE | re.UNICODE)
-    
-    for place in places:
-        text = re.sub(re.escape(place), place_placeholder, text, flags=re.IGNORECASE | re.UNICODE)
-
-    return text
+    matches = re.findall(age_pattern, text)
+    for match in matches:
+        masked_word = "{japanese_age}"
+        masked_text = masked_text.replace(match, masked_word)
+        if match not in replacements:
+            replacements[match] = masked_word
+    return masked_text, replacements
 
 
-def apply_japanese_filters(text, filters=[  
-                    filter_japanese_by_name_keisyou,
-                    filter_japanese_by_place_keisyou,
-                    filter_japanese_by_age_keyshou]):
-#                    filter_japanese_by_mecab]):
+def apply_japanese_filters(text, filters=[
+                            filter_japanese_by_name_keisyou,
+                            filter_japanese_by_place_keisyou,
+                            filter_japanese_by_age_keyshou,
+                            filter_japanese_names]):
+    replacements = {}
     for filter_func in filters:
-        text = filter_func(text)
-    return text
+        text, filter_replacements = filter_func(text)
+        replacements.update(filter_replacements)
+    return text, replacements
+
+import json
+
+def decode_text(json_string, replacements):
+    try:
+        if json_string:
+            data = json.loads(json_string)
+            if isinstance(data, dict):
+                content = data.get("content", "")
+                for key, value in replacements.items():
+                    content = content.replace(value, key)
+                data["content"] = content
+                return json.dumps(data)
+        return json_string
+    except (json.JSONDecodeError, TypeError):
+        return json_string
+
+# def decode_text(json_string, replacements):
+#     # Load the JSON string into a Python dictionary
+#     data = json.loads(json_string)
+
+#     # Invert the replacements dictionary
+#     inverted_replacements = {v: k for k, v in replacements.items()}
+
+#     # If the content is nested within the dictionary, you may need to go deeper:
+#     content = data['content']
+
+#     # Replace placeholders with original values
+#     for placeholder, original in inverted_replacements.items():
+#         content = content.replace(placeholder, original)
+
+#     # Replace the original content with the decoded content
+#     data['content'] = content
+
+#     # Convert the Python dictionary back into a JSON string
+#     json_string = json.dumps(data)
+
+#     return json_string
 
 
-def apply_filters(text, filters= [ 'numerical', 'keyword',  'anonymize'],jp_filters=[] ,model=None, keyword_file=None):
+def filter_confidential_by_ai(text, model):
+    # This is a placeholder for AI-based text filtering
+    # You will need to replace this with your actual AI model for filtering confidential information.
+    # Let's assume that your AI model works in a way that it returns a list of words or phrases
+    # that are considered confidential in the text. 
 
-    if filters== [] :
-        filters= [ 'numerical', 'keyword',  'anonymize', 'japanese']
+    confidential_info = model.predict(text) # Placeholder for your model's method
+    replacements = {}
+    for word in confidential_info:
+        masked_word = "{confidential_info}"
+        text = text.replace(word, masked_word)
+        if word not in replacements:
+            replacements[word] = masked_word
+
+    return text, replacements
+
+
+def apply_filters(text, filters=['numerical', 'keyword', 'anonymize'], jp_filters=[], model=None, keyword_file=None):
+    replacements = {}
+    
+    if filters == []:
+        filters = ['numerical', 'keyword', 'anonymize', 'japanese']
 
     if 'numerical' in filters:
-        text = filter_numerical_information(text)
+        text, numerical_replacements = filter_numerical_information(text)
+        #replacements.update(numerical_replacements)
+        unique_update(replacements, numerical_replacements)
+
     if 'keyword' in filters and keyword_file:
-        text = filter_keyword_information(keyword_file, text)
+        text, keyword_replacements = filter_keyword_information(text, keyword_file)
+        #replacements.update(keyword_replacements)
+        unique_update(replacements, keyword_replacements)
+
     if 'anonymize' in filters:
-        text = filter_anonymize_text(text)
+        text, anonymize_replacements = filter_anonymize_text(text)
+        #replacements.update(anonymize_replacements)
+        unique_update(replacements, anonymize_replacements)
+
     if 'japanese' in filters:
         if jp_filters == []:
-            text = apply_japanese_filters(text)
+            text, japanese_replacements = apply_japanese_filters(text)
         else:
-            text = apply_japanese_filters(text, filters=jp_filters)
+            text, japanese_replacements = apply_japanese_filters(text, filters=jp_filters)
+        #replacements.update(japanese_replacements)
+        unique_update(replacements, japanese_replacements)
+
+    if 'japanese_names' in filters:
+        text, japanese_names_replacements = filter_japanese_names(text)
+        #replacements.update(japanese_names_replacements)
+        unique_update(replacements, japanese_names_replacements)
+
     if 'ai' in filters and model:
-        text = filter_confidential_by_ai(text, model)
-    
-    return text
-
-
-def filter_numerical_information(text):
-    # Replace phone numbers
-    text = re.sub(r'\b\d{2,4}[-\s.]?\d{2,4}[-\s.]?\d{2,4}[-\s.]?\d{0,4}\b', '{phone_number}', text)
-    
-    # Replace credit card numbers
-    text = re.sub(r'\b(?:\d{4}[-\s.]?){3}\d{4}\b', '{credit_card_number}', text)
-    
-    # Replace expiration dates
-    text = re.sub(r'\b\d{2}\/\d{2,4}\b', '{expiration_date}', text)
-
-    return text
+        text, ai_replacements = filter_confidential_by_ai(text, model)
+        #replacements.update(ai_replacements)
+        unique_update(replacements, ai_replacements)
+    return text, replacements
 
 
 def contains_confidential_information(message):
@@ -260,7 +378,7 @@ class SendMessages(graphene.Mutation):
 
            # Apply filters to the user_message
         if user_message:
-            user_message = apply_filters(user_message, filters=['numerical', 'keyword', 'anonymize'], jp_filters=[], model=None, keyword_file=None)
+            user_message, replacements = apply_filters(user_message, filters=['numerical', 'keyword', 'anonymize'], jp_filters=[], model=None, keyword_file=None)
 
 
         # Add new messages to session history and input_messages for model
@@ -275,6 +393,23 @@ class SendMessages(graphene.Mutation):
 
         # Call the model with the combined old and new input_messages
         response = chat_with_models(model_name, input_messages)
+
+        # Iterate over each message in the response
+        for message in response:
+            # Check if the message is from the assistant
+            if message['role'] == 'assistant':
+                # Decode the assistant's message and update the content in the response
+                message['content'] = decode_text(message['content'], replacements)
+        
+        # # Parse the JSON response and decode the content
+        # response = json.loads(response.text)
+        # decoded_content = decode_text(response['content'], replacements)
+
+        # # Replace the original content with the decoded content
+        # response['content'] = decoded_content
+
+        # # Convert the Python dictionary back into a JSON string
+        # response = json.dumps(response)
 
         if isinstance(response, list):
             system_message = next((msg["content"] for msg in response if msg["role"] == "system"), None)
